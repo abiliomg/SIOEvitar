93e06b9c49919d57385679277f7b2d8030b6f557
abiliomg <44577191+abiliomg@users.noreply.github.com>
2020-06-04 22:59:03 +0100

    mediaCompras

--- SIO/routes/Transaction.route.js
+++ SIO/routes/Transaction.route.js
@@ -4,4 +4,5 @@ var TransactionController = require("../controllers/Transaction.controller.js");
 
 router.get('/', TransactionController.getAllTransaction);
 router.get('/comprasMes/:year',TransactionController.getComprasPorMes);
+router.get('/mediaComprasMes/:year',TransactionController.getMediaComprasPorMes);
 module.exports = router;
